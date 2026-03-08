148a98710c93b83a997f35bf5c96754451aa53ae
Marwan Aouida <marwan@aouida.com>
2026-03-08 10:47:24 +0100

    sketcher: move without arguments go to origin

--- lib/core/2d/move.ts
+++ lib/core/2d/move.ts
@@ -1,15 +1,16 @@
-import { Point2DLike } from "../../math/point.js";
+import { Point2D, Point2DLike } from "../../math/point.js";
 import { Move } from "../../features/2d/move.js";
 import { normalizePoint2D } from "../../helpers/normalize.js";
 import { registerBuilder, SceneParserContext } from "../../index.js";
 
 interface MoveFunction {
+  (): Move;
   (to: Point2DLike): Move;
 }
 
 function build(context: SceneParserContext): MoveFunction {
   return function move() {
-    const to = normalizePoint2D(arguments[0]);
+    const to = normalizePoint2D(arguments[0] ?? new Point2D(0, 0));
     const move = new Move(to)
     context.addSceneObject(move);
 
